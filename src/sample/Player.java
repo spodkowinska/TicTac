9e137090451aab228e92ed3127b1e140acb8398e
SPodkowinska <spodkowinska@gmail.com>
2020-12-02 14:33:58 +0100

    sorting positions

--- src/sample/Player.java
+++ src/sample/Player.java
@@ -6,16 +6,19 @@ import javafx.scene.image.ImageView;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Player {
     int number;
-    ArrayList<Point> positions;
+    SortedSet<Point> positions;
     public boolean isCurrentPlayer;
 
 
     public Player(int number) {
         this.number = number;
-        this.positions = new ArrayList<>();
+        this.positions = new TreeSet<>((p1,p2)->p1.getX() == p2.getX() ? (int)(p2.getY() - p1.getY()) : (int)(p2.getX() - p1.getX()));
     }
 
     public int getNumber() {
@@ -31,46 +34,53 @@ public class Player {
     }
 
     public void addPosition(Point point) {
-        for(int i = 0; i<positions.size(); i++){
+//        for (int i = 0; i < positions.size(); i++) {
+//            if (positions.get(i).getX() > point.getX()) {
+//                for (int j = 0; j < positions.size(); j++) {
+//                    if (positions.get(j).getY() > point.getY()) {
+//                        positions.add(i, point);
+//                    }
+//                }
+//            }
+//        }
 
-            if(positions.get(i).getX()>point.getX()){
-                if(positions.get(i).getY()>point.getY()){
-                    positions.add(i,point);
-                }
-            }
-        }
         this.positions.add(point);
+//        positions.sort((p1,p2)->p1.getX() == p2.getX() ? (int)(p2.getY() - p1.getY()) : (int)(p2.getX() - p1.getX()));
+
     }
+    
 
     public boolean isWinner() {
-        if (positions.size() > 2) {
-            if (positions.size()==5){
-                if (positions.get(0).getX() - positions.get(2).getX() == positions.get(2).getX() - positions.get(4).getX()
-                        && positions.get(0).getY() - positions.get(2).getY() == positions.get(2).getY() - positions.get(4).getY()) {
+
+        ArrayList<Point> points = new ArrayList<Point>(positions);
+        if (points.size() > 2) {
+            if (points.size() == 5) {
+                if (points.get(0).getX() - points.get(2).getX() == points.get(2).getX() - points.get(4).getX()
+                        && points.get(0).getY() - points.get(2).getY() == points.get(2).getY() - points.get(4).getY()) {
                     return true;
                 }
-                if (positions.get(0).getX() - positions.get(3).getX() == positions.get(3).getX() - positions.get(4).getX()
-                        && positions.get(0).getY() - positions.get(3).getY() == positions.get(3).getY() - positions.get(4).getY()) {
+                if (points.get(0).getX() - points.get(3).getX() == points.get(3).getX() - points.get(4).getX()
+                        && points.get(0).getY() - points.get(3).getY() == points.get(3).getY() - points.get(4).getY()) {
                     return true;
                 }
-                if (positions.get(0).getX() - positions.get(1).getX() == positions.get(1).getX() - positions.get(4).getX()
-                        && positions.get(0).getY() - positions.get(1).getY() == positions.get(1).getY() - positions.get(4).getY()) {
-                    return true;
-                }
-            }
-            if (positions.size() >= 4) {
-                if (positions.get(0).getX() - positions.get(2).getX() == positions.get(2).getX() - positions.get(3).getX()
-                        && positions.get(0).getY() - positions.get(2).getY() == positions.get(2).getY() - positions.get(3).getY()) {
-                    return true;
-                }
-                if (positions.get(0).getX() - positions.get(1).getX() == positions.get(1).getX() - positions.get(3).getX()
-                        && positions.get(0).getY() - positions.get(1).getY() == positions.get(1).getY() - positions.get(3).getY()) {
+                if (points.get(0).getX() - points.get(1).getX() == points.get(1).getX() - points.get(4).getX()
+                        && points.get(0).getY() - points.get(1).getY() == points.get(1).getY() - points.get(4).getY()) {
                     return true;
                 }
             }
-            if (positions.size() >= 3) {
-                if (positions.get(0).getX() - positions.get(1).getX() == positions.get(1).getX() - positions.get(2).getX()
-                        && positions.get(0).getY() - positions.get(1).getY() == positions.get(1).getY() - positions.get(2).getY()) {
+            if (points.size() >= 4) {
+                if (points.get(0).getX() - points.get(2).getX() == points.get(2).getX() - points.get(3).getX()
+                        && points.get(0).getY() - points.get(2).getY() == points.get(2).getY() - points.get(3).getY()) {
+                    return true;
+                }
+                if (points.get(0).getX() - points.get(1).getX() == points.get(1).getX() - points.get(3).getX()
+                        && points.get(0).getY() - points.get(1).getY() == points.get(1).getY() - points.get(3).getY()) {
+                    return true;
+                }
+            }
+            if (points.size() >= 3) {
+                if (points.get(0).getX() - points.get(1).getX() == points.get(1).getX() - points.get(2).getX()
+                        && points.get(0).getY() - points.get(1).getY() == points.get(1).getY() - points.get(2).getY()) {
                     return true;
                 }
             }
